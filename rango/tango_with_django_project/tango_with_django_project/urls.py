e4649df4c93c29b5df1a5ca5495b11f589d64cee
Harry Chung <harry@Harrys-MBP.lan1>
2019-02-03 18:22:32 -0800

    added script to enter new items into db

--- rango/tango_with_django_project/tango_with_django_project/urls.py
+++ rango/tango_with_django_project/tango_with_django_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index')
-]
+    path('rango/', views.index, name='index'),
+    path('rango/about/', views.about, name='about')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
